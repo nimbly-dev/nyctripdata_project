feea12ea90ba3f6855c046ce7e50216c8234d932
Altheo Saquilayan <altheo.saquilayan@exist.com>
2024-11-11 18:57:34 +0800

    DATAENG-23: Added first working workflow

--- de_zoomcamp_nyc_taxi/start_runner.sh
+++ de_zoomcamp_nyc_taxi/start_runner.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-# Navigate to the GitHub runner directory
+export GITHUB_API_URL="https://api.github.com"
+export PAT="$GITHUB_PAT"
+export OWNER="nimbly-dev"
+export REPO="nyctripdata_project"
+
+# Fetch the token directly and check
+response=$(curl -s -X POST -H "Authorization: token $PAT" "$GITHUB_API_URL/repos/$OWNER/$REPO/actions/runners/registration-token")
+TOKEN=$(echo "$response" | jq -r .token)
+
+if [ "$TOKEN" == "null" ]; then
+    echo "Failed to retrieve a valid token. Response was: $response"
+    exit 1
+fi
+
+# Run config with the token
 cd /actions-runner
-
-# Configure the GitHub runner
-./config.sh --url $GITHUB_URL \
-            --token $GITHUB_TOKEN \
-            --name "$RUNNER_NAME" \
-            --work "$RUNNER_WORKDIR" \
-            --unattended \
-            --replace
-
-# Start the runner
+./config.sh --url "https://github.com/$OWNER/$REPO" --token "$TOKEN" --unattended --replace
 ./run.sh
